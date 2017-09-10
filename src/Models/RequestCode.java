521085b4eb02dbb9f7c8746ef46cfd387b1751e3
Pamela Peixinho <pamela.peixinho@quintoandar.com.br>
2017-09-10 01:53:47 -0300

    create request class and serilazble communicate with server

---
+++ src/Models/RequestCode.java
@@ -0,0 +1,53 @@
+package Models;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by pamelaiupipeixinho on 10/09/17.
+ */
+public class RequestCode implements Serializable {
+    private String query;
+    private String language;
+    private List<String> libs;
+    private List<String> comments;
+
+    public RequestCode(String query, String language, List<String> libs, List<String> comments) {
+        this.query = query;
+        this.language = language;
+        this.libs = libs;
+        this.comments = comments;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public List<String> getLibs() {
+        return libs;
+    }
+
+    public void setLibs(List<String> libs) {
+        this.libs = libs;
+    }
+
+    public List<String> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<String> comments) {
+        this.comments = comments;
+    }
+}
--- src/Network/Client.java
+++ src/Network/Client.java
@@ -1,11 +1,14 @@
 package Network;
 
+import Models.RequestCode;
+import com.google.gson.Gson;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import io.socket.client.IO;
 import io.socket.client.Socket;
 
 import java.net.URISyntaxException;
+import java.util.Collections;
 
 /**
  * Created by pamelaiupipeixinho on 10/09/17.
@@ -14,18 +17,32 @@ public class Client extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        System.out.print("GOING TO CONNECT");
+        System.out.println("GOING TO CONNECT");
         Socket socket = null;
         try {
             socket = IO.socket("http://0.0.0.0:6060/code-recommendations");
             Socket finalSocket = socket;
             socket.on(Socket.EVENT_CONNECT, args13 -> {
-                System.out.print("CONECTADO");
-                finalSocket.emit("foo", "hi");
+                System.out.println("CONECTADO");
+                RequestCode rc = new RequestCode("read file",
+                        "Python",
+                        Collections.singletonList("os"),
+                        Collections.singletonList("open file to read file"));
+
+                Gson gson = new Gson();
+                Object request = gson.toJson(rc);
+
+                finalSocket.emit("getCodes", request);
 //                finalSocket.disconnect();
             }).on("event", args1 -> {
-                System.out.print("EVENT");
-            }).on(Socket.EVENT_DISCONNECT, args12 -> {});
+                System.out.println("EVENT");
+            }).on(Socket.EVENT_DISCONNECT, args12 -> {})
+            .on("recommendationCodes", args -> {
+//                JSONObject obj = (JSONObject)args[0];
+                System.out.println("receive call");
+                finalSocket.disconnect();
+            });
+
             socket.connect();
         } catch (URISyntaxException e1) {
             e1.printStackTrace();
