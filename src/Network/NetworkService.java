d38da7d2c8d7d92d0c9a47ea55e8f06e09da0c92
pamepeixinho <pameiupip@gmail.com>
2017-10-23 00:34:38 -0200

    remove opts

--- src/Network/NetworkService.java
+++ src/Network/NetworkService.java
@@ -31,12 +31,12 @@ public class NetworkService {
                                       Runnable errorFunction) {
         System.out.println("GOING TO CONNECT");
         try {
-            IO.Options opts = new IO.Options();
-            opts.reconnection = true;
-            opts.reconnectionDelay = 1000;
-            opts.timeout = 25;
+//            IO.Options opts = new IO.Options();
+//            opts.reconnection = true;
+//            opts.reconnectionDelay = 1000;
+//            opts.timeout = 25;
 //            this.socket = IO.socket("http://apollo.gwachs.com:10443/code-recommendations");
-            this.socket = IO.socket("http://0.0.0.0:10443/code-recommendations", opts);
+            this.socket = IO.socket("http://0.0.0.0:10443/code-recommendations");
             Socket finalSocket = this.socket;
             socket.on(Socket.EVENT_CONNECT, args13 -> {
                 System.out.println("CONECTADO");
