a4ec911d1a2bb60d35e32a13a37d1147f4d34f51
pamepeixinho <pameiupip@gmail.com>
2017-11-14 14:45:42 -0200

    update plugins

--- src/Network/NetworkService.java
+++ src/Network/NetworkService.java
@@ -28,8 +28,8 @@ public class NetworkService {
                                       Runnable errorFunction) {
         System.out.println("GOING TO CONNECT");
         try {
-            this.socket = IO.socket("http://0.0.0.0:10443/code-recommendations");
-//            this.socket = IO.socket("https://quickslothrecommendationserver.herokuapp.com/code-recommendations");
+//            this.socket = IO.socket("http://0.0.0.0:10443/code-recommendations");
+            this.socket = IO.socket("https://quickslothrecommendationserver.herokuapp.com/code-recommendations");
             Socket finalSocket = this.socket;
             socket.on(Socket.EVENT_CONNECT, args13 -> {
                 System.out.println("CONECTADO");
